e4db87c748dcbd2ca55f11b65164167bde1a0053
JRuiz1601 <1110366598@u.icesi.edu.co>
2024-11-18 19:06:55 -0500

    create frontend project using React

---
+++ online-store-frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Invoices from './pages/Invoices';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ online-store-frontend/src/pages/Register.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        // SI DA FALLO CAMBIAR A 3000
+      const response = await axios.post('http://localhost:5000/api/users/register', formData);
+      alert('Registro exitoso');
+      navigate('/login'); // Redirige al login tras el registro exitoso
+    } catch (err) {
+      setError(err.response.data.error || 'Error al registrarse');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Registro</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nombre</label>
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
+        <div>
+          <label>Contraseña</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+        </div>
+        <button type="submit">Registrarse</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
